1a6735890475d5208062220404ad0b6758625d82
David Boucha <boucha@gmail.com>
2012-05-24 23:40:50 -0600

    We moved the python path up a level, update paths
    The paths to the python install was updated to reflect
    recent change to remove a level of folders

--- deps/salt/salt-install-service.bat
+++ deps/salt/salt-install-service.bat
@@ -1,1 +1,1 @@
-"C:\\salt\\python\\Portable Python 2.7.2.1\\App\\python" "C:\\salt\\python\\Portable Python 2.7.2.1\\App\\Scripts\\salt-minion" -c "c:\\salt\\etc\\salt\\minion" -d
+"C:\\salt\\python\\App\\python" "C:\\salt\\python\\App\\Scripts\\salt-minion" -c "c:\\salt\\etc\\salt\\minion" -d
