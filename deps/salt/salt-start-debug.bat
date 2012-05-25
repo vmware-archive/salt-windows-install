ab4c7601b826719ebbfa0b4deb7b41c9428c991d
David Boucha <boucha@gmail.com>
2012-05-24 22:27:44 -0600

    Add debug start batch file
    - update paths
    - fix typo

---
+++ deps/salt/salt-start-debug.bat
@@ -0,0 +1,1 @@
+"C:\\salt\\python\\Portable Python 2.7.2.1\\App\\python" "C:\\salt\\python\\Portable Python 2.7.2.1\\App\\Scripts\\salt-minion" -c "c:\\salt\etc\\salt\\minion" -l debug
--- deps/salt/update-salt.bat
+++ deps/salt/update-salt.bat
@@ -4,4 +4,4 @@ set SaltGitRepo=Salt\salt-github\salt
 
 %SaltDriveLetter%
 cd %SaltGitRepo%
-"C:\salt\python\Portable Python 2.7.2.1\App\python" setup.py install --prefix="C:\\salt\\python\\Portable Python 2.7.2.1\\App\\"
+"C:\\salt\\python\\Portable Python 2.7.2.1\\App\\python" setup.py install --prefix="C:\\salt\\python\\Portable Python 2.7.2.1\\App\\"
