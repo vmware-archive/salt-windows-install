d6ff0a6f97284815646ecedf1c67dc7a2d1f6435
David Boucha <boucha@gmail.com>
2012-05-28 22:13:30 -0600

    Fix working directory so win32api imports
    Change to c:\salt\python\App so that imports
    work correctly

--- deps/salt/salt-start-debug.bat
+++ deps/salt/salt-start-debug.bat
@@ -1,1 +1,4 @@
+C:
+cd \salt\python\App
+
 "C:\\salt\\python\\App\\python" "C:\\salt\\python\\App\\Scripts\\salt-minion" -c "c:\\salt\\etc\\salt\\minion" -l debug
--- deps/salt/salt-start.bat
+++ deps/salt/salt-start.bat
@@ -1,1 +1,3 @@
+c:
+cd \salt\python\App
 "C:\\salt\\python\\App\\python" "C:\\salt\\python\\App\\Scripts\\salt-minion" -c "c:\\salt\\etc\\salt\\minion"
